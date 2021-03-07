05c9e66acb97fcd53774936d4f872f6fe3e1f850
tedsmoore <tedsmoore@gmail.com>
2021-03-07 18:48:07 -0500

    roll my own login required functionality to make custom response

--- users/htmx.py
+++ users/htmx.py
@@ -1,21 +1,17 @@
 from django.http import HttpResponse
+from django.core.exceptions import PermissionDenied
 from django.views.generic.base import View
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from users.models import Player
 
 
-class PlayersAPIViews(LoginRequiredMixin, View):
+class PlayersAPIViews(View):
 
-    def get(self, request):
-        user = Player.objects.get(id=request.user.id)
-        data = request.GET.dict()
-
-        if data.get('follow'):
-            to_follow = data.get('follow')
-            user.following.add(id=to_follow)
-
-        return
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            # todo: this can return a nice modal to prompt sign in, make a base class
+            raise PermissionDenied("You need to sign in to access this feature")
+        return super().dispatch(request, *args, **kwargs)
 
     def post(self, request):
         user = Player.objects.get(id=request.user.id)
@@ -27,7 +23,8 @@ class PlayersAPIViews(LoginRequiredMixin, View):
 
         return HttpResponse('<i class="fas fa-check-circle" style="color:#4CAF50; margin:auto;"></i>')
 
-    def _follow_unfollow(self, user, to_follow):
+    @staticmethod
+    def _follow_unfollow(user, to_follow):
         if user.following.filter(id=to_follow).exists():
             user.following.remove(to_follow)
             resp = '<i class="far fa-circle" style="color:dimgrey; margin:auto;"></i>'
