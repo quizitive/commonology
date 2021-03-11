6137fa2a7fefa2f3b69b1f254b798ee3efbf7202
tedsmoore <tedsmoore@gmail.com>
2021-03-10 20:12:52 -0500

    cleanup css enable follow filter

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 
 from users.views import user_logout, profile_view, join_view, \
-    email_confirmed_view, send_invite_view, LoginView
+    email_confirmed_view, send_invite_view
 from users.htmx import PlayersHTMXView
 from users.forms import LoginForm
 
