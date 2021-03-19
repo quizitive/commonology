01edf4658afda29b1c2586605f05d95f34899a51
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-03-19 15:51:48 -0400

    First release of mailchimp_hook for unsubscribe.

--- project/settings.py
+++ project/settings.py
@@ -153,11 +153,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = False
+# USE_I18N = True
+# USE_L10N = True
+# USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
