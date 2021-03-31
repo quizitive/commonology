22649287f265dc8bb600e67202113ad96e0bf7ec
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-03-31 14:17:34 -0400

    User.save() now adds to mailchimp list.

--- mail/tasks.py
+++ mail/tasks.py
@@ -1,6 +1,9 @@
 from celery import shared_task
 from project import settings
 from .mailchimp_utils import Mailchimp
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
@@ -10,5 +13,8 @@ def add_to_mailchimp(email, is_subscribed=True):
                           list_id=settings.MAILCHIMP_EMAIL_LIST_ID)
     if is_subscribed:
         status_code, status = mc_client.subscribe(email)
+        logger.info(f"{email} is subcribed in Mailchimp with {status_code} / {status}")
+
     else:
         status_code, status = mc_client.unsubscribe(email)
+        logger.info(f"{email} is unsubcribed in Mailchimp with {status_code} / {status}")
