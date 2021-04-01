570ce34f3c70487c2b2ca3a4345bb86991468af0
tedsmoore <tedsmoore@gmail.com>
2021-03-31 20:13:24 -0400

    need event listeners on base html page

--- users/static/users/js/htmx_config.js
+++ users/static/users/js/htmx_config.js
@@ -1,15 +1,5 @@
-window.onload = function () {
-  document.body.addEventListener('htmx:configRequest', (event) => {
-    event.detail.headers['X-CSRFToken'] = '{{ csrf_token }}';
-  })
-  document.body.addEventListener('htmx:responseError', (event) => {
-    if (event.detail.error.search("Error Code 403") !== -1) {
-      loginPrompt();
-    }
-  })
-}
-
 // Handle 403
 function loginPrompt() {
+  console.log('prompting login');
   document.getElementById('login-modal').style.display='block';
 }
