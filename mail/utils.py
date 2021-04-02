4789d1ff7d4ab2f20a8e297f27d62e67479ece05
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-04-02 13:12:02 -0400

    Removed debug print statement.

--- mail/utils.py
+++ mail/utils.py
@@ -3,7 +3,6 @@ from .mailchimp_utils import Mailchimp
 
 
 def get_mc_client():
-    print(settings.MAILCHIMP_EMAIL_LIST_ID)
     return Mailchimp(settings.MAILCHIMP_SERVER,
                      settings.MAILCHIMP_API_KEY,
                      list_id=settings.MAILCHIMP_EMAIL_LIST_ID)
