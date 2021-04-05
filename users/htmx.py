0db0bab4690542cace32d8472e13bf585425f5a9
tedsmoore <tedsmoore@gmail.com>
2021-04-05 11:00:03 -0400

    reacquiring local changes after revert

--- users/htmx.py
+++ users/htmx.py
@@ -1,13 +1,8 @@
-from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.core.exceptions import PermissionDenied
-from django.contrib.auth import get_user_model
 from django.views.generic.base import View
 
-from users.models import PendingEmail
-
-from .utils import remove_pending_email_invitations, send_invite
-
-User = get_user_model()
+from users.models import Player
 
 
 class PlayersHTMXView(View):
@@ -18,7 +13,7 @@ class PlayersHTMXView(View):
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request):
-        user = User.objects.get(id=request.user.id)
+        user = Player.objects.get(id=request.user.id)
         data = request.POST.dict()
 
         if data.get('to_follow'):
@@ -35,24 +30,3 @@ class PlayersHTMXView(View):
             user.following.add(to_follow)
         user.save()
         return HttpResponse()
-
-
-class InviteFriendsHTMXView(View):
-
-    def post(self, request, *args, **kwargs):
-        data = request.POST.dict()
-        emails = data.get('email')
-        responses = []
-        for email in emails:
-            try:
-                User.objects.get(email=email)
-                # can't join if user exists
-                responses.append(HttpResponse(f"User {email} already exists", status_code=409))
-            except (User.DoesNotExist):
-                remove_pending_email_invitations()
-                pe = PendingEmail(email=email, referrer=request.user.email)
-                pe.save()
-                send_invite(request, pe)
-                responses.append(HttpResponse(f"Invite successfully sent to {email}."))
-
-        return JsonResponse({'responses': responses})
