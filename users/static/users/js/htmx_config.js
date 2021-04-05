138378332839b9d615c8151c1818f2c7f7b9fae5
tedsmoore <tedsmoore@gmail.com>
2021-04-05 10:39:02 -0400

    reverting master

--- users/static/users/js/htmx_config.js
+++ users/static/users/js/htmx_config.js
@@ -1,12 +1,3 @@
-// function showHideDropdown() {
-//   const drpdn = document.getElementById("navbar-dropdown-content")
-//   if (drpdn.classList.contains("w3-show")) {
-//     drpdn.classList.remove("w3-show")
-//   } else {
-//     drpdn.classList.add("w3-show")
-//   }
-// }
-
 // Handle 403
 function loginPrompt() {
   console.log('prompting login');
