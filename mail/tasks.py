9ab8ef5e3d5753255e2ae0336d2123209a8c014f
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-04-08 22:00:14 -0400

    First draft, needs some tests.

--- mail/tasks.py
+++ mail/tasks.py
@@ -12,10 +12,12 @@ def update_mailing_list(email, is_subscribed=True):
     if is_subscribed:
         status_code, status = mc_client.subscribe(email)
     else:
-        status_code, status = mc_client.unsubscribe(email)
+        # status_code, status = mc_client.unsubscribe(email)
+        status = 'archived'
+        status_code = mc_client.archive(email)
 
     msg = f"{email} is {status} in Mailchimp with {status_code}"
-    if 200 == status_code:
+    if status_code in (200, 204):
         logger.info(msg)
     else:
         logger.error(msg)
