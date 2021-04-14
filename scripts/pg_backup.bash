9629e9a6866e8cfaaa946738445ff61010a6254b
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-04-14 17:56:50 -0400

    Removed secret_env reference from deploy.bash

--- scripts/pg_backup.bash
+++ scripts/pg_backup.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -a
+source /etc/profile.d/django_project.sh
+set +a
+
 export DAYOFWEEK=$(date +"%a")
 pg_dump -h localhost -F t $DBNAME > $PG_DUMP_DIR/"$DBNAME"_"$DAYOFWEEK.tar"
 
