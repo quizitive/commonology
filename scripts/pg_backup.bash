128021dbfd2180c26a7e12d5be13a49e24757ede
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-04-14 18:02:04 -0400

    Added -U option to pg_dump in pg_backup.bash.

--- scripts/pg_backup.bash
+++ scripts/pg_backup.bash
@@ -5,7 +5,7 @@ source /etc/profile.d/django_project.sh
 set +a
 
 export DAYOFWEEK=$(date +"%a")
-pg_dump -h localhost -F t $DBNAME > $PG_DUMP_DIR/"$DBNAME"_"$DAYOFWEEK.tar"
+pg_dump -h localhost -F t -U postgres $DBNAME > $PG_DUMP_DIR/"$DBNAME"_"$DAYOFWEEK.tar"
 
 # To restore:
 # pg_restore --clean --create --no-acl --no-owner -d $DBNAME $DBNAME_$DAYOFWEEK.tar
