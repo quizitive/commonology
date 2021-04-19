d43b6171bb3c263c8388bdebe315cf65c5b04214
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-04-19 11:20:30 -0400

    Starting to use sendgrid to replace mailchimp.

--- scripts/pg_update_dev_db.zsh
+++ scripts/pg_update_dev_db.zsh
@@ -1,17 +1,20 @@
 #!/usr/bin/env zsh
 
-if [ -z "$COMMONOLOGY_DEV_DIR" ]; then
+SSH_SRC="django@commonologygame.com"
+DEV_DIR="$HOME/Documents/dev"
+PROJECT="commonology"
+PROJECT_DEV_DIR="$DEV_DIR/$PROJECT"
+
+if [ -z "$PROJECT_DEV_DIR" ]; then
   echo "You must set the DJANGO_DEV_DIR env var."
   echo "Something like this:"
-  echo "export COMMONOLOGY_DEV_DIR=\"$HOME/Documents/dev/commonology\""
+  echo "export PROJECT_DEV_DIR=\"$HOME/Documents/dev/commonology\""
   exit
 fi
 
-cd $COMMONOLOGY_DEV_DIR/pg_dumps
-rsync -avz -e ssh django@commonologygame.com:~/pg_dumps $COMMONOLOGY_DEV_DIR/
+
+rsync -avz -e ssh $SSH_SRC:~/pg_dumps $PROJECT_DEV_DIR/
+cd $PROJECT_DEV_DIR/pg_dumps
 fn=`ls -d *(om[1])`
 echo $fn
-/usr/local/bin/pg_restore --verbose --clean --no-acl --no-owner -d commonology $fn
-
-
-
+/usr/local/bin/pg_restore --verbose --clean --no-acl --no-owner -d $PROJECT $fn
