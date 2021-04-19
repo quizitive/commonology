57499bab1f4b94bd7f9c22330683d68d3749502d
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-04-19 12:32:56 -0400

    Merge branch 'master' of github.com:quizitive/commonology into sendgrid118 # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- mail/admin.py
+++ mail/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from .models import MassMailMessage
+from django_object_actions import DjangoObjectActions
 
 
 @admin.register(MassMailMessage)
-class MassMailMessageAdmin(admin.ModelAdmin):
+class MassMailMessageAdmin(DjangoObjectActions, admin.ModelAdmin):
+    def send_test(self, request, obj):
+        obj.tested = True
+        obj.save()
+        print("Testing", obj.test_recipient)
+    send_test.label = 'Test'
+    send_test.short_description = "Send a test message."
+
+    def blast(self, request, obj):
+        if obj.tested:
+            print("Blasting", obj.subject)
+        else:
+            print("Cannot send blast until message was tested.")
+    blast.label = "Blast"
+    blast.short_description = "This will send the message to EVERYONE!"
+
+    change_actions = ('send_test', 'blast')
+
     list_display = ('created', 'subject', 'test_recipient')
     list_filter = ('created',)
     search_fields = ('subject',)
