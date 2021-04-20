45db3f35f68f30094c07e03d19d9669291a4ef57
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-04-20 10:43:07 -0400

    Added subscribe/unsubscribe admin actions.

---
+++ users/actions.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.db import transaction
+
+
+def un_sub(modeladmin, request, qs, subscribed):
+    with transaction.atomic():
+        for u in qs:
+            u.subscribed = subscribed
+            u.save()
+
+
+def unsubscribe_action(modeladmin, request, qs):
+    un_sub(modeladmin, request, qs, False)
+
+
+unsubscribe_action.description = "Unsubscribe"
+
+
+def subscribe_action(modeladmin, request, qs):
+    un_sub(modeladmin, request, qs, True)
+
+
+subscribe_action.description = "Subscribe"
