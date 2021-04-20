af531c75dd92084b5ce3d5fcd5ccbeccc015fcd2
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-04-20 08:49:58 -0400

    pep8.

--- project/urls.py
+++ project/urls.py
@@ -19,4 +19,4 @@ urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('', views.index, name='home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-              static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
