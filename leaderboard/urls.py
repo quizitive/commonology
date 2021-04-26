ccb2f6a4a06c1e98b0aa787b68c3f45435e39c89
tedsmoore <tedsmoore@gmail.com>
2021-04-25 20:38:03 -0400

    a results view with space for custom commentary

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -6,6 +6,6 @@ from leaderboard import htmx
 urlpatterns = [
     path('', views.LeaderboardView.as_view(), name='leaderboard'),
     path('<int:game_id>/', views.LeaderboardView.as_view(), name='leaderboard-game'),
-    path('results/<int:game_id>/', views.ResultsView.as_view(), name='results'),
+    path('results/', views.ResultsView.as_view(), name='results'),
     path('htmx/', htmx.LeaderboardHTMXView.as_view(), name='leaderboard-htmx')
 ]
