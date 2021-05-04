233c0ccfa4d383b444133b25a508df2ad2a2712d
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-05-04 12:43:10 -0400

    Some changes to support remote development.

--- scripts/pg_update_dev_db.zsh
+++ scripts/pg_update_dev_db.zsh
@@ -5,6 +5,14 @@ DEV_DIR="$HOME/Documents/dev"
 PROJECT="commonology"
 PROJECT_DEV_DIR="$DEV_DIR/$PROJECT"
 
+DBNAME=$PROJECT
+if [ $# -ge 1 ]; then
+  DBNAME=$1
+else
+  echo "You can use this for dev databases like: pg_update_dev_d.zsh <dbname>"
+fi
+echo "DBNAME=$DBNAME"
+
 if [ -z "$PROJECT_DEV_DIR" ]; then
   echo "PROJECT_DEV_DIR did not get set properly."
   echo "Check the vars set at the top of this script."
@@ -17,6 +25,6 @@ rsync -avz -e ssh $SSH_SRC:~/pg_dumps $PROJECT_DEV_DIR/
 cd $PROJECT_DEV_DIR/pg_dumps
 fn=`ls -d *(om[1])`
 echo $fn
-psql -U postgres -h localhost -c "DROP DATABASE \"$PROJECT\";"
-psql -U postgres -h localhost -c "CREATE DATABASE \"$PROJECT\";"
-pg_restore --verbose --clean --no-acl --no-owner -h 127.0.0.1 -d $PROJECT $fn
+psql -U postgres -h localhost -c "DROP DATABASE \"$DBNAME\";"
+psql -U postgres -h localhost -c "CREATE DATABASE \"$DBNAME\";"
+pg_restore --verbose --clean --no-acl --no-owner -h 127.0.0.1 -d $DBNAME $fn
