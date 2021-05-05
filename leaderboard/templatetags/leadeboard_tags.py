c51ea98564925e4071156676a590cb0ba51cf0f0
tedsmoore <tedsmoore@gmail.com>
2021-05-05 16:33:29 -0400

    clean up admin and migration

--- leaderboard/templatetags/leadeboard_tags.py
+++ leaderboard/templatetags/leadeboard_tags.py
@@ -10,3 +10,11 @@ def question_responses(context):
         return context['answer_tally'][context['q'].text]
     except KeyError:
         return {}
+
+
+@register.simple_tag(takes_context=True)
+def button_highlight(context, dest_link):
+    if context['view'] == dest_link:
+        return 'cg-blue'
+    else:
+        return 'w3-light-grey'
