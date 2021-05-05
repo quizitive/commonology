5a056f5b5bfb71df78ef52dd1312798cecc3de2c
tedsmoore <tedsmoore@gmail.com>
2021-05-04 21:41:03 -0400

    score form now has dropdown of available series

--- game/forms.py
+++ game/forms.py
@@ -2,6 +2,13 @@ from django import forms
 
 
 class TabulatorForm(forms.Form):
+
+    def __init__(self, series_choices):
+        super().__init__()
+        self.fields['series'].widget.choices = [("", "")] + series_choices
+        self.fields['series'].initial = "Please Select"
+
+    series = forms.CharField(widget=forms.Select(attrs={'class': 'w3-input'}), required=True)
     sheet_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'w3-input'}))
     update_existing = forms.BooleanField(
         label="Update existing answer records (slower) ",
