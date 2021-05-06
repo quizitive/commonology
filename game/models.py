dae1bdf42cd89549fe393d604f8b4f73f804ade6
tedsmoore <tedsmoore@gmail.com>
2021-05-06 16:27:56 -0400

    players property should use a game question to filter

--- game/models.py
+++ game/models.py
@@ -77,7 +77,8 @@ class Game(models.Model):
 
     @property
     def players(self):
-        return self.game_questions.first().raw_answers.values(
+        return self.game_questions.exclude(
+            type__in=(Question.op, Question.ov)).first().raw_answers.values(
             'player', 'player__display_name').annotate(
             is_host=models.Case(
                 models.When(player__in=self.hosts.values_list('id', flat=True), then=True),
