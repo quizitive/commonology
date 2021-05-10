a39747c43ae14db74f9fa41fcc4f582cabc0ecfa
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-05-10 12:27:53 -0400

    Eliminating mailchimp hooks and code.

--- mail/views.py
+++ mail/views.py
@@ -21,29 +21,3 @@ def mailtest(request):
 
     return render(request, 'mail/mailtest.html',
                   {'message': message, "emailaddr": emailaddr})
-
-
-@method_decorator(csrf_exempt, name='dispatch')
-class MailchimpWebhook(View):
-
-    def get(self, request, uuid=None):
-        # Need this for Mailchimp to validate hook when setting up an audience.
-        return HttpResponse("OK")
-
-    def post(self, request, uuid):
-        assert(uuid == settings.MAILCHIMP_HOOK_UUID)
-
-        data = request.POST
-        list_id = data['data[list_id]']
-        assert(list_id == str(settings.MAILCHIMP_EMAIL_LIST_ID))
-
-        email = data['data[email]']
-        action = data.get('type', 'unknown')
-        if action == 'unsubscribe':
-            unsubscribe(email)
-        elif action == 'subscribe':
-            subscribe(email)
-        elif action == 'archived':
-            unsubscribe(email)
-
-        return HttpResponse("OK")
