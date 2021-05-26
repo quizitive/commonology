233f56f8438c228b0a6aefd3cd13f5e22cd27bb6
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-05-26 17:54:55 -0400

    POC that worked on staging.commonologygame.com.

--- project/asgi.py
+++ project/asgi.py
@@ -8,13 +8,20 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
+import django
+
+django.setup()
+
+
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from chat.urls import websocket_urlpatterns
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
