bfa3578dd62826c5144e1ddd1647bfa3075cedd9
tedsmoore <tedsmoore@gmail.com>
2021-06-04 13:11:16 -0400

    add a filter for removed comments

--- chat/admin.py
+++ chat/admin.py
@@ -10,10 +10,10 @@ class CommentAdmin(admin.ModelAdmin):
     # todo: this will break when we have threads on anything not associated with a game
     # when that happens, a more robust solution would be implementing a custom ListFilter
     # see https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_filter
-    list_filter = ('thread__object__game',)
+    list_filter = ('removed', 'thread__object__game',)
+
     search_fields = ('player__email__exact', 'player__display_name', 'comment', 'removed', 'thread__object__game__name')
 
     def status(self, obj):
         return not obj.removed
     status.boolean = True
-
