25c5dfd92b5c5101c0fbd807da10e54a7c434d31
tedsmoore <tedsmoore@gmail.com>
2021-06-04 13:05:32 -0400

    an admin to manage comments

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat.models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('commenter', 'comment', 'status')
+
+    # todo: this will break when we have threads on anything not associated with a game
+    # when that happens, a more robust solution would be implementing a custom ListFilter
+    # see https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_filter
+    list_filter = ('thread__object__game',)
+    search_fields = ('player__email__exact', 'player__display_name', 'comment', 'removed', 'thread__object__game__name')
+
+    def status(self, obj):
+        return not obj.removed
+    status.boolean = True
+
