a7a8ea8a3e6babeebbb71c95648b479b11e90585
tedsmoore <tedsmoore@gmail.com>
2021-06-11 14:48:54 -0400

    drier and single-purpose functions

--- game/urls.py
+++ game/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('score/', views.tabulator_form_view, name='tabulator_form'),
     path('play/', views.GameEntryWithoutValidationView.as_view(), name='play'),
     path('play/<uidb64>', views.GameEntryValidationView.as_view(), name='player_confirm'),
-    path('game/', views.GameFormView.as_view(), name='game_form')
+    path('game/', views.GameFormView.as_view(), name='game-form'),
+    path('game/<int:game_id>/', views.GameFormView.as_view(), name='game-id-form')
 ]
