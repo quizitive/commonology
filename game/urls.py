9380e5be3e0a5fa68bd3450e9544dcd2881e3c60
tedsmoore <tedsmoore@gmail.com>
2021-06-23 20:33:04 -0400

    wip: game form and answer view, creating direct access method

--- game/urls.py
+++ game/urls.py
@@ -6,7 +6,8 @@ app_name = 'game'
 urlpatterns = [
     path('score/', views.tabulator_form_view, name='tabulator_form'),
     path('play/', views.GameEntryView.as_view(), name='play'),
-    path('play/<uidb64>', views.GameEntryValidationView.as_view(), name='player_confirm'),
+    path('play/<uidb64>/', views.GameEntryValidationView.as_view(), name='player_confirm'),
     # path('my-answers/', views.GameFormView.as_view(), name='game-form'),
-    path('my-answers/<int:game_id>', views.GameFormView.as_view(), name='game-id-form')
+    path('game/<int:game_id>/', views.GameFormView.as_view(), name='game-form'),
+    path('game/<int:game_id>/<str:player_answers_code>/', views.GameFormView.as_view(), name='game-view')
 ]
