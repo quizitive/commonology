ef84813315d4a163c08745cc543842849ef83663
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-06-29 12:05:18 -0400

    Play flow (#326)
    * Half baked - committing for the day.
    
    * Half baked - committing for the day.
    
    * Half baked - committing for the day.
    
    * Half baked - committing for the day.
    
    * Half baked - committing for the day.
    
    * Basic functionality working - working on tests now.
    
    * Basic functionality working - working on tests now.
    
    * Basic functionality working - working on tests now.
    
    * Added notes to enable daphne.
    
    * First release, ready for manual testing.
    
    * Added test of bad play uuid.
    
    * Better messages when game is not found.
    
    * Better messages when game is not found.
    
    * Updated email confirm template.
    
    * Updated email confirm template.
    
    * Added login button to play card.

--- game/urls.py
+++ game/urls.py
@@ -5,6 +5,7 @@ app_name = 'game'
 
 urlpatterns = [
     path('score/', views.tabulator_form_view, name='tabulator_form'),
-    path('play/', views.GameEntryWithoutValidationView.as_view(), name='play'),
-    path('play/<uidb64>', views.GameEntryValidationView.as_view(), name='player_confirm'),
+    path('play/', views.GameEntryView.as_view(), name='play'),
+    path('play/<game_uuid>', views.GameEntryView.as_view(), name='uuidplay'),
+    path('play/<game_uuid>/<pending_uuid>', views.GameEntryValidationView.as_view(), name='player_confirm'),
 ]
