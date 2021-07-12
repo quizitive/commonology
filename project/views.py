b7ab579d12cf902cc8800a8d90e30a29bf92b53e
tedsmoore <tedsmoore@gmail.com>
2021-07-12 11:48:08 -0400

    wip: stats and charts

--- project/views.py
+++ project/views.py
@@ -2,6 +2,7 @@ from django.views.generic.base import View
 from django.shortcuts import render, redirect
 from django import forms
 from django.views.generic.edit import FormMixin
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.mail import send_mail
 from game.utils import next_event, find_latest_public_game
@@ -105,6 +106,12 @@ class CardFormView(FormMixin, View):
         return self.render(request)
 
 
+class CardChartView(CardFormView):
+
+    header = "A Chart"
+
+
+
 class ContactForm(forms.Form):
     choices = (("1", "Game Host"), ("2", "Investor Relations"))
     to_email = ['concierge@commonologygame.com', 'ms@quizitive.com']
@@ -139,3 +146,99 @@ class ContactView(CardFormView):
                            form_method="get", form_action='/')
 
         return self.render(request)
+
+
+from pychartjs import BaseChart, ChartType, Color
+
+
+class ChartJS(BaseChart):
+
+    def __init__(self, data_class, name, **kwargs):
+        self.data_class = data_class
+        self.name = name
+        self.kwargs = kwargs
+
+    @property
+    def data_name(self):
+        return self.name + "-data"
+
+    @property
+    def data(self):
+        return self.data_class(**self.kwargs)
+
+
+class BaseChartDataClass:
+
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            self.__setattr__(k, v)
+        self.data = self._data()
+
+    def _data(self):
+        raise NotImplementedError
+
+
+class ExampleDataClass(BaseChartDataClass):
+
+    def _data(self):
+        if self.game_id == 1:
+            return [12, 19, 3, 17, 10]
+        return [1, 4, 13, 12, 9]
+
+
+class ExampleDataClass2(BaseChartDataClass):
+
+    def _data(self):
+        if self.game_id == 1:
+            return [12, 19, 3, 17, 10]
+        return [1, 4, 13, 12, 9]
+
+
+class ExampleTwoDatasetDataClass(BaseChartDataClass):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.dataset1_1 = self._dataset_1()
+
+    class Dataset1(BaseChartDataClass):
+        def _data(self):
+            if self.game_id == 1:
+                return [12, 19, 3, 17, 10]
+            return [1, 4, 13, 12, 9]
+
+    class Dataset2(BaseChartDataClass):
+        def _data(self):
+            if self.game_id == 1:
+                return [12, 19, 3, 17, 10]
+            return [1, 4, 13, 12, 9]
+
+
+class GamePlayers(BaseChartDataClass):
+
+    def _data(self):
+        pass
+
+
+class WeeklyPlayers(BaseChartDataClass):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.backgroundColor = Color.Green
+
+    def _data(self):
+        if self.game_id == 1:
+            return [12, 19, 3, 17, 10]
+        return [1, 4, 13, 12, 9]
+
+
+
+@login_required
+def stats_view(request):
+    chart_1 = ChartJS(ExampleDataClass, name="myChart", game_id=1, backgroundColor=Color.Green)
+    chart_2 = ChartJS(ExampleDataClass, name="chart_2", game_id=2)
+    context = {
+        "custom_message": "Trend of the percent of game players that are members",
+        "chart_1": chart_1,
+        "chart_2": chart_2,
+    }
+    return render(request, 'stats.html', context)
