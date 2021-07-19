481d4eb1a4c06d8f82905598a970c114f39b003c
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-07-19 18:33:46 -0400

    Remove destroy_for_testing.py

--- scripts/hacks/destroy_for_testing.py
+++
@@ -1,9 +0,0 @@
-
-from game.models import Answer
-from chat.models import Comment
-from users.models import PendingEmail, Player
-
-Answer.objects.all().delete()
-Comment.objects.all().delete()
-PendingEmail.objects.all().delete()
-Player.objects.filter(is_staff=False).all().delete()
