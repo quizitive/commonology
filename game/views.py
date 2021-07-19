accf21fb6762049184bc250f02b131857c256ba7
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-07-19 14:21:01 -0400

    Refactored referrer field in Player and PendingEmail model to be foreign keys.

--- game/views.py
+++ game/views.py
@@ -269,9 +269,10 @@ def game_url(google_form_url, email):
     return google_form_url.replace('alex@commonologygame.com', email)
 
 
-def send_confirm(request, g, email):
+def send_confirm(request, g, email, referrer_id=None):
     remove_pending_email_invitations()
-    pe = PendingEmail(email=email)
+
+    pe = PendingEmail(email=email, referrer_player_id=referrer_id)
     pe.save()
 
     slug = g.series.slug
@@ -316,6 +317,8 @@ class GameEntryView(PSIDMixin, CardFormView):
         slug = kwargs.get('series_slug') or 'commonology'
         game_uuid = kwargs.get('game_uuid')
         user = request.user
+
+        referrer_id = request.GET.get('r', user.id)
 
         if not game_uuid:
             g = find_latest_public_game(slug)
@@ -389,7 +392,8 @@ class GameEntryView(PSIDMixin, CardFormView):
 
         email = request.POST['email']
 
-        send_confirm(request, g, email)
+        referrer_id = request.GET.get('r')
+        send_confirm(request, g, email, referrer_id)
         custom_message = mark_safe(f"<b>We sent the game link to {email}. </b>"
                               f"Don't forget to check your spam or junk folder if need be. "
                               f"By the way, if you were logged in you'd be playing already.")
