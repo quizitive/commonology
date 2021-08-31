ee047bcd72d9b6e828c744f4fc743ae44b8299bc
tedsmoore <tedsmoore@gmail.com>
2021-08-30 22:54:26 -0400

    take ttl arg on decorator, force refresh on call

--- project/utils.py
+++ project/utils.py
@@ -30,32 +30,30 @@ def redis_delete_patterns(*patterns):
     return total_keys_deleted
 
 
-def quick_cache(func):
+def quick_cache(ttl=600):
     """
     A decorator to cache the result of any function by its name and arguments. Example usage:
 
-    @quick_cache
+    @quick_cache(ttl=300)
     def foo(a, b):
         return a + b
 
-    foo(1, 2) ->  check redis for key `foo:1_2`, return cache result if found, otherwise execute foo and set value
-    foo(1, 2, timeout=60 * 60) -> same as above, set explicit timeout
-    foo(1, 2, force_refresh=True) -> always execute foo and set value, default timeout
-    foo(1, 2, force_refresh=True, timeout=60 * 60) -> also works
+    > foo(1, 2) ->  checks redis for key `foo:1_2`, returns cache result if found, otherwise execute foo and set value
+    > foo(1, 2, force_refresh=True) -> always execute foo and set value
 
     NOTE: Make sure arguments are unique! If passing a model instance, make
     sure the model has a unique __repr__ defined.
     """
-
-    @functools.wraps(func)
-    def wrapper(*args, force_refresh=False, timeout=600, **kwargs):
-        redis_key = f'{func.__name__}:' + "_".join([repr(a) for a in args]) + "_".join(repr(k) for k in kwargs.values())
-        if not force_refresh:
-            cached_res = REDIS.get(redis_key)
-            if cached_res:
-                return cached_res
-        res = func(*args, **kwargs)
-        REDIS.set(redis_key, res, timeout=timeout)
-        return res
-
-    return wrapper
+    def inner(func):
+        @functools.wraps(func)
+        def wrapper(*args, force_refresh=False, **kwargs):
+            redis_key = f'{func.__name__}:' + "_".join([repr(a) for a in args]) + "_".join(repr(k) for k in kwargs.values())
+            if not force_refresh:
+                cached_res = REDIS.get(redis_key)
+                if cached_res:
+                    return cached_res
+            res = func(*args, **kwargs)
+            REDIS.set(redis_key, res, timeout=ttl)
+            return res
+        return wrapper
+    return inner
