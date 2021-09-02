b2dad201bb424cd1628f6d2975d35e5cb8a83d90
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-09-02 17:22:47 -0400

    Logic works, now we have to smooth out the language and formatting.

--- rewards/views.py
+++ rewards/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from project.card_views import CardFormView
 from rewards.forms import ClaimForm
+from rewards.models import MailingAddress, Claim
 
 
 class ClaimView(LoginRequiredMixin, CardFormView):
@@ -11,7 +12,27 @@ class ClaimView(LoginRequiredMixin, CardFormView):
     header = "Claim Reward"
     button_label = 'Submit'
 
+    def get_form(self, form_class=None):
+        player = self.request.user
+        try:
+            a = MailingAddress.objects.get(player=player)
+        except MailingAddress.DoesNotExist:
+            a = None
+        form = self.form_class(instance=a, data=self.request.POST or None)
+        return self.format_form(form)
+
     def get(self, request, *args, **kwargs):
+        player = request.user
+        if Claim.objects.filter(player=player).exists():
+            m = "You have already claimed your mug.  Sorry, but we are limited one per customer."
+            self.header = "Claim staked!"
+            return self.info(request,
+                             message=m,
+                             form=None,
+                             form_method='get',
+                             form_action=f'/',
+                             button_label='Thank you!')
+
         messages.info(request, "Please fill out this form so you can enjoy a hot drink in this beautiful mug.")
         return super().get(request, *args, **kwargs)
 
@@ -23,9 +44,14 @@ class ClaimView(LoginRequiredMixin, CardFormView):
             messages.error(request, "There was a problem saving your changes. Please try again.")
             return self.render(request)
 
+        player = request.user
+
         address = form.save(commit=False)
-        address.player = request.user
+        address.player = player
         form.save()
+
+        c = Claim(player=player)
+        c.save()
 
         m = "We'll send your prize ASAP!"
 
@@ -33,4 +59,6 @@ class ClaimView(LoginRequiredMixin, CardFormView):
         return self.info(request,
                          message=m,
                          form=None,
-                         form_method='get')
+                         form_method='get',
+                         form_action=f'/',
+                         button_label='Thank you!')
