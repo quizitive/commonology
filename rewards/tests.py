ce519bb7ecdb3ad062b856c3526800b95a46e8f5
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-09-03 14:35:36 -0400

    Added some tests to rewards app.

--- rewards/tests.py
+++ rewards/tests.py
@@ -16,15 +16,17 @@ project.utils.slackit = slackit
 
 class RewardTests(TestCase):
     def setUp(self):
-        player = get_local_user()
-        q = Question.objects.create(text='question text')
-        for i in range(10):
-            e = f"player{i}@localhost.com"
-            p = get_local_user(e=e)
-            p.referrer = player
-            p.save()
-            Answer.objects.create(player=p, raw_string='answer', question=q)
-        self.player = player
+        self.player = get_local_user()
+        self.q = Question.objects.create(text='question text')
+        for i in range(9):
+            self.make_referee(i)
+
+    def make_referee(self, i):
+        e = f"player{i}@localhost.com"
+        p = get_local_user(e=e)
+        p.referrer = self.player
+        p.save()
+        Answer.objects.create(player=p, raw_string='answer', question=self.q)
 
     def test_referrer_count(self):
         self.assertEqual(self.player.players_referred.count(), 10)
@@ -32,11 +34,17 @@ class RewardTests(TestCase):
     def test_claim(self):
         client = get_local_client()
         path = reverse('claim')
+
+        response = client.get(path)
+        self.assertEqual(response.reason_phrase, 'OK')
+        self.assertContains(response, "It seems you have not made 10 referrals yet")
+
+        self.make_referee(10)
+
         response = client.get(path)
         self.assertEqual(response.reason_phrase, 'OK')
         self.assertContains(response, "Please fill out this form so you can enjoy a hot drink in this beautiful mug.")
 
-        path = reverse('claim')
         data = {'Full Name': 'NORMAL', 'Address line 1': '1 First Street', 'City': 'Patchogue',
                 'State': 'NY', 'ZIP / Postal code': '12345'}
         response = client.post(path, data=data)
