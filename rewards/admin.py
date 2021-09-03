0ea9537202fcd8ffb442e55556e35c77e01ad8b5
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-09-03 12:46:52 -0400

    Check number of referrals before allowing claim.

--- rewards/admin.py
+++ rewards/admin.py
@@ -2,4 +2,8 @@ from rewards.models import MailingAddress, Claim
 from django.contrib import admin
 
 admin.site.register(MailingAddress)
-admin.site.register(Claim)
+
+
+@admin.register(Claim)
+class ClaimAdmin(admin.ModelAdmin):
+    readonly_fields = ('Claim date',)
