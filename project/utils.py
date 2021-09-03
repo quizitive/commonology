c133212445f64df3149e03d96f12506411230b79
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-09-03 11:56:51 -0400

    Added some comments about slackit().

--- project/utils.py
+++ project/utils.py
@@ -64,6 +64,7 @@ def slackit(msg):
     # Documentation: https://api.slack.com/web
     # App ID: A02D8J3T1S9
     # Manage App here: https://api.slack.com/apps/A02D8J3T1S9/general
+    # Can get oath token here: https://api.slack.com/apps/A02D8J3T1S9/oauth
     url = f"https://hooks.slack.com/services/{settings.SLACK_TOKEN}"
     headers = {'content-type': 'application/json'}
     data = {'text': msg}
