5e5468e310f156ad685e879002b2a521583f6664
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-09-06 17:55:20 -0400

    Link address to rewards claim admin list.

--- rewards/admin.py
+++ rewards/admin.py
@@ -1,8 +1,13 @@
 from rewards.models import MailingAddress, Claim
 from django.contrib import admin
 from django.contrib.admin import SimpleListFilter
+from django.utils.safestring import mark_safe
+from rewards.models import MailingAddress
 
-admin.site.register(MailingAddress)
+
+@admin.register(MailingAddress)
+class MailingAddressAdmin(admin.ModelAdmin):
+    list_display = ('player', )
 
 
 class SentFilter(SimpleListFilter):
@@ -23,4 +28,15 @@ class SentFilter(SimpleListFilter):
 class ClaimAdmin(admin.ModelAdmin):
     readonly_fields = ('claim_date',)
     list_filter = (SentFilter,)
-    list_display = ('player', 'claim_date', 'sent_date')
+    list_display = ('player', 'claim_date', 'sent_date', 'address')
+    readonly_fields = ['address']
+
+    def address(self, obj):
+        try:
+            a = MailingAddress.objects.get(player=obj.player)
+            return mark_safe(f"<a href='/admin/rewards/mailingaddress/{a.pk}/change/'>address</a>")
+        except MailingAddress.DoesNotExist:
+            return "missing"
+
+    # the following is necessary if 'link' method is also used in list_display
+    address.allow_tags = True
