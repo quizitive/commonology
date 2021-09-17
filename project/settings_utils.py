eeb4a75373da90cc5826ccda64e3ae22dd084ca0
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-09-17 18:54:46 -0400

    Finding int of mtime for latest css file.

--- project/settings_utils.py
+++ project/settings_utils.py
@@ -1,14 +1,19 @@
 import os
-from project import settings
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 def find_latest_css():
-    root = settings.BASE_DIR
+    root = BASE_DIR
     fns = [os.path.join(root, entry) for
            root, dirs, files in os.walk(root) for
            entry in dirs + files if
            entry.endswith('.css')]
+
     substring_list = ['static/fontawesome', 'static/ckeditor', 'static/admin']
+
     t = [os.path.getmtime(f) for f in fns if
          not any(map(f.__contains__, substring_list))]
     t = max(t)
