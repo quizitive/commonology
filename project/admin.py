fe29254b81571c482806c6067dff4613d8ed64e5
Ted Moore <tedsmoore@gmail.com>
2021-09-25 10:25:06 -0400

    Custom admin mockup (#484)
    * wip: custom admin mockup
    
    * custom admin that actually has some helpful links
    
    * quick links to stats and game rules pages, move stats page into admin
    
    * remove dead code
    
    * game status summary template tag
    
    * use the content_title block

---
+++ project/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render
+from django.template.response import TemplateResponse
+
+from game.charts import PlayerTrendChart, PlayersAndMembersDataset
+
+
+class CommonologyAdmin(admin.AdminSite):
+    index_template = 'admin/index.html'
+    enable_nav_sidebar = True
+
+    def get_urls(self):
+        urls = super().get_urls()
+        other_urls = [path('stats/', self.admin_view(self.stats_view), name='game-stats')]
+        return other_urls + urls
+
+    def stats_view(self, request):
+        chart_1 = PlayerTrendChart(
+            PlayersAndMembersDataset, slug='commonology', name="chart_3", since_game=38)
+        chart_2 = PlayerTrendChart(
+            PlayersAndMembersDataset, slug='commonology', name="chart_2", since_game=38, agg_period=4)
+        context = dict(
+            # Include common variables for rendering the admin template.
+            self.each_context(request),
+            title='Stats'
+        )
+        context.update({
+            "chart_1": chart_1,
+            "chart_2": chart_2,
+        })
+        return render(request, 'game/stats.html', context)
--- project/templatetags/project_tags.py
+++ project/templatetags/project_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from project.settings import BUILD_NUMBER
+from game.utils import players_vs_previous, find_latest_active_game
 
 register = template.Library()
 
@@ -7,3 +8,12 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def bn(context):
     return BUILD_NUMBER
+
+
+@register.simple_tag
+def current_game_overview():
+    current_game = find_latest_active_game('commonology')
+    cur, prev, pct_chg = players_vs_previous(current_game)
+    action_terms = ('so far', 'at this point') if current_game else ('', '')
+    return f'{cur} players {action_terms[0]} this week, compared to {prev} {action_terms[1]} last week. ' \
+           f'That represents a change of {pct_chg:.2f}%.'
