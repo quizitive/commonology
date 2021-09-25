641ac6a6172535e45e03c6f554332f28ca153d40
Ted Moore <tedsmoore@gmail.com>
2021-09-25 10:47:26 -0400

    Use most recently started game every time (#510)
    * wip: custom admin mockup
    
    * custom admin that actually has some helpful links
    
    * quick links to stats and game rules pages, move stats page into admin
    
    * remove dead code
    
    * game status summary template tag
    
    * use the content_title block
    
    * convenience function for most recently started game, with is_active flag

--- project/templatetags/project_tags.py
+++ project/templatetags/project_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from project.settings import BUILD_NUMBER
-from game.utils import players_vs_previous, find_latest_active_game
+from game.utils import players_vs_previous, most_recently_started_game
 
 register = template.Library()
 
@@ -12,8 +12,8 @@ def bn(context):
 
 @register.simple_tag
 def current_game_overview():
-    current_game = find_latest_active_game('commonology')
+    current_game, is_active = most_recently_started_game('commonology')
+    action_terms = ('so far', 'at this point') if is_active else ('', '')
     cur, prev, pct_chg = players_vs_previous(current_game)
-    action_terms = ('so far', 'at this point') if current_game else ('', '')
     return f'{cur} players {action_terms[0]} this week, compared to {prev} {action_terms[1]} last week. ' \
            f'That represents a change of {pct_chg:.2f}%.'
