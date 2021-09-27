ccdde291edd53d1d11e2b8161d62e37acbb9ee58
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-09-27 18:39:07 -0400

    Making v_static template tag that does static but adds version number based on mtime.

--- project/templatetags/project_tags.py
+++ project/templatetags/project_tags.py
@@ -1,13 +1,31 @@
+import os
+from pathlib import Path
 from django import template
+from django.templatetags.static import do_static, StaticNode
 from project.settings import BUILD_NUMBER
 from game.utils import players_vs_previous, most_recently_started_game
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def bn(context):
     return BUILD_NUMBER
+
+
+@register.tag('v_static')
+def bn_do_static(parser, token):
+    bits = token.split_contents()
+    path = parser.compile_filter(bits[1])
+    path = str(path).strip('\"')
+    fn = os.path.join(BASE_DIR, 'project/static', path)
+    t = os.path.getmtime(fn)
+    v = int(t)
+    result = do_static(parser, token)
+    result.path.token += f"?v={v}"
+    return result
 
 
 @register.simple_tag
