de1865446820e589aa6dc2b22377ba299760e5d5
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-10-06 14:30:32 -0400

    Modifed mv_referrals.py hack to fix referrals for tessa.burke@meltwater.com.

--- scripts/hacks/mv_referrals.py
+++ scripts/hacks/mv_referrals.py
@@ -11,10 +11,39 @@ django.setup()
 
 from users.models import Player
 
+if False:
+    from_player = Player.objects.get(email='bluesutf201@gnail.com')
+    to_player = Player.objects.get(email='bluesurf201@gmail.com')
 
-from_player = Player.objects.get(email='bluesutf201@gnail.com')
-to_player = Player.objects.get(email='bluesurf201@gmail.com')
+    for referee in from_player.players_referred:
+        referee.referrer = to_player
+        referee.save()
 
-for referee in from_player.players_referred:
-    referee.referrer = to_player
-    referee.save()
+
+if True:
+    to_player = Player.objects.get(email='tessa.burke@meltwater.com')
+    froms = [Player.objects.get(email=e) for e in ['kellydawnmendoza@gmail.com',
+                                                   'maggiestanford20@gmail.com',
+                                                   'gpiccoletti@gmail.com',
+                                                   'dustin.mannino@meltwater.com',
+                                                   'zach.baker@meltwater.com']]
+    for referee in froms:
+        referee.referrer = to_player
+        referee.save()
+
+    for p in to_player.players_referred:
+        print(p)
+
+    # Rose Mendoza
+    # Kelly Mendoza, kellydawnmendoza@gmail.com
+    # Trevor Ells - OK
+    # Caroline Montgomery -- OK
+    # Maggie Stanford -- maggiestanford20@gmail.com
+    # Nabila Mahmud -- OK
+    # Gianni Pickles -- gpiccoletti@gmail.com
+    # Jordan Williams --- OK
+    # Dustin Mannino -- dustin.mannino@meltwater.com
+    # Fred Walker -- OK
+    # Zach Baker -- zach.baker@meltwater.com
+    # Gail Robertson -- OK
+    # Andres Ferret -- OK
