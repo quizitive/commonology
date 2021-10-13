41e9c32c27222631871339f0beec72df9b900ee3
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-10-13 16:58:33 -0400

    Revert "Added update_requirements.py script."
    This reverts commit 29fcf85b942588f018f7312f330fe4aab465f87d.

--- scripts/update_requirements.py
+++ scripts/update_requirements.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
-import os
 import sys
 import subprocess
 
 # This script will install all the packages in requirement.txt without pegging versions.
 # Then it will list new versions installed for an updated requirements.txt file.
 
-# Run like this:
-#      $ scripts/update_requirements.py > new_requirements.txt
-#      $ cat new_requirements.txt > requirements.txt
-
-# To remove all packages: $ pip uninstall -y -r <(pip freeze)
+# How to remove all packages? `pip uninstall -y -r <(pip freeze)`
 
 
 with open('requirements.txt', 'r') as fh:
@@ -20,12 +15,10 @@ git_packages = [i.strip() for i in lines if 'git+http' in i]
 
 required_packages = [i.split('=')[0].strip() for i in lines if i.strip()]
 
-with open(os.devnull, 'w') as devnull:
-    x = subprocess.call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'],
-                        stderr=devnull, stdout=devnull)
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
 
-    for package_name in required_packages:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package_name], stderr=devnull, stdout=devnull)
+for package_name in required_packages:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package_name])
 
 reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
 installed_packages = [r.decode() for r in reqs.split()]
