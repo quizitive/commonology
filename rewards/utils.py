cf90f73991fe0d6528c1803f15b0ab2e967b813a
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-10-17 19:53:27 -0400

    Starting to create winner certificates.

--- rewards/utils.py
+++ rewards/utils.py
@@ -33,3 +33,74 @@ def check_for_reward(player):
     if referrer:
         if settings.REWARD_THRESHOLD == referrer.players_referred.count():
             send_reward_notice(referrer)
+
+def replace_text(content, replacements = dict()):
+    lines = content.splitlines()
+
+    result = ""
+    in_text = False
+
+    for line in lines:
+        if line == "BT":
+            in_text = True
+
+        elif line == "ET":
+            in_text = False
+
+        elif in_text:
+            cmd = line[-2:]
+            if cmd.lower() == 'tj':
+                replaced_line = line
+                for k, v in replacements.items():
+                    replaced_line = replaced_line.replace(k, v)
+                result += replaced_line + "\n"
+            else:
+                result += line + "\n"
+            continue
+
+        result += line + "\n"
+
+    return result
+
+
+def process_data(object, replacements):
+    data = object.getData()
+    decoded_data = data.decode('utf-8')
+    # decoded_data = data.decode("utf-8", "ignore")
+
+    replaced_data = replace_text(decoded_data, replacements)
+
+    encoded_data = replaced_data.encode('utf-8')
+    if object.decodedSelf is not None:
+        object.decodedSelf.setData(encoded_data)
+    else:
+        object.setData(encoded_data)
+
+
+def write_certificate():
+    # https://stackoverflow.com/questions/17742042/how-to-fill-pdf-form-in-python
+    # https://stackoverflow.com/questions/41769120/search-and-replace-for-text-within-a-pdf-in-python
+
+    from io import BytesIO
+    import PyPDF2
+    from PyPDF2.generic import BooleanObject, NameObject, IndirectObject, NumberObject
+
+    replacements = {'Joshua Louis Simon': 'Marc Schwarzschild'}
+
+    pdf_template = "project/static/img/WinnerCertificate.pdf"
+    input_stream = open(pdf_template, "rb")
+    pdf_reader = PyPDF2.PdfFileReader(ippnput_stream, strict=False)
+    pdf_writer = PyPDF2.PdfFileWriter()
+
+    page = pdf_reader.getPage(0)
+    # content = page.extractText()
+    contents = page.getContents()
+    process_data(contents, replacements)
+
+    page[NameObject("/Contents")] = contents.decodedSelf
+
+    pdf_writer.addPage(page)
+
+
+    with open("marc.pdf", 'wb') as out_file:
+        pdf_writer.write(out_file)
