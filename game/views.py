bf55bb0f8f5144e3780291f6cc348490e4c512b7
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-10-19 16:05:16 -0400

    Validate player won game.

--- game/views.py
+++ game/views.py
@@ -622,7 +622,7 @@ class AwardCertificate(LoginRequiredMixin, BaseGameView):
 
     def get(self, request, game_id, *args, **kwargs):
         player = request.user
-        if player in winners_of_game(self.game):
+        if  player.is_superuser or (player in winners_of_game(self.game)):
             # game_number = kwargs.get('game_id')
             game_number = self.game.game_id
             name = player.display_name
