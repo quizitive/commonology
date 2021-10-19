dbca213496b7fbdc4112c6b8ac83ca8a2d88cb20
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-10-19 12:15:41 -0400

    Early working version.

--- rewards/utils.py
+++ rewards/utils.py
@@ -1,7 +1,11 @@
+import os
+import subprocess
 from django.template.loader import render_to_string
 from project import settings
+from project.utils import our_now
 from mail.utils import send_one
 from project.utils import slackit
+
 
 import logging
 
@@ -34,73 +38,32 @@ def check_for_reward(player):
         if settings.REWARD_THRESHOLD == referrer.players_referred.count():
             send_reward_notice(referrer)
 
-def replace_text(content, replacements = dict()):
-    lines = content.splitlines()
 
-    result = ""
-    in_text = False
+def write_winner_certificate(name, date, game_number):
+    path = settings.WINNER_ROOT
+    pdf_template = "rewards/templates/rewards/WinnerCertificate.pdf"
 
-    for line in lines:
-        if line == "BT":
-            in_text = True
+    fn_base = f"{name}{date}{game_number}.{our_now()}".replace(',', '').replace(' ', '').strip()
+    fn_base = os.path.join(path, fn_base)
 
-        elif line == "ET":
-            in_text = False
+    fdf = f'''
+        %FDF-1.2
+        1 0 obj << /FDF << /Fields [
+        << /T(Name) /V({name}) >>
+        << /T(Date) /V({date}) >>
+        << /T(Game \#) /V({game_number}) >>
+        ] >> >>
+        endobj
+        trailer
+        << /Root 1 0 R >>
+        %%EOF
+    '''
 
-        elif in_text:
-            cmd = line[-2:]
-            if cmd.lower() == 'tj':
-                replaced_line = line
-                for k, v in replacements.items():
-                    replaced_line = replaced_line.replace(k, v)
-                result += replaced_line + "\n"
-            else:
-                result += line + "\n"
-            continue
+    fn_fdf = fn_base + '.fdf'
+    with open(fn_fdf, 'w') as fh:
+        fh.write(fdf)
 
-        result += line + "\n"
-
-    return result
-
-
-def process_data(object, replacements):
-    data = object.getData()
-    decoded_data = data.decode('utf-8')
-    # decoded_data = data.decode("utf-8", "ignore")
-
-    replaced_data = replace_text(decoded_data, replacements)
-
-    encoded_data = replaced_data.encode('utf-8')
-    if object.decodedSelf is not None:
-        object.decodedSelf.setData(encoded_data)
-    else:
-        object.setData(encoded_data)
-
-
-def write_certificate():
-    # https://stackoverflow.com/questions/17742042/how-to-fill-pdf-form-in-python
-    # https://stackoverflow.com/questions/41769120/search-and-replace-for-text-within-a-pdf-in-python
-
-    from io import BytesIO
-    import PyPDF2
-    from PyPDF2.generic import BooleanObject, NameObject, IndirectObject, NumberObject
-
-    replacements = {'Joshua Louis Simon': 'Marc Schwarzschild'}
-
-    pdf_template = "project/static/img/WinnerCertificate.pdf"
-    input_stream = open(pdf_template, "rb")
-    pdf_reader = PyPDF2.PdfFileReader(ippnput_stream, strict=False)
-    pdf_writer = PyPDF2.PdfFileWriter()
-
-    page = pdf_reader.getPage(0)
-    # content = page.extractText()
-    contents = page.getContents()
-    process_data(contents, replacements)
-
-    page[NameObject("/Contents")] = contents.decodedSelf
-
-    pdf_writer.addPage(page)
-
-
-    with open("marc.pdf", 'wb') as out_file:
-        pdf_writer.write(out_file)
+    fn = fn_base + '.pdf'
+    p = subprocess.run(['pdftk', pdf_template, 'fill_form', fn_fdf, 'output',  fn, 'flatten'])
+    print(p.returncode == 0)
+    print(f'open {fn}')
