cea80a85b4466cc508d258d43a5f37eb612c59db
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-10-19 15:53:42 -0400

    Moved from rewards to game app.

--- rewards/urls.py
+++ rewards/urls.py
@@ -4,5 +4,4 @@ from rewards import views
 
 urlpatterns = [
     path("claim/", views.ClaimView.as_view(), name="claim"),
-    path("award_certificate/<int:n>/", views.AwardCertificate.as_view(), name='award_certificate'),
 ]
