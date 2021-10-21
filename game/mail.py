2bd8b00e5e86d5f75091a611c5f7d2efc4fdb5dc
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-10-21 17:09:11 -0400

    Adding admin action to send award certificates.

--- game/mail.py
+++ game/mail.py
@@ -11,11 +11,11 @@ def send_winner_notice(winner, game_number):
     slackit(f"Sending winner notification to {winner}.")
     try:
         email_context = {
-            'url': f'https://commonologygame.com/award_certficate/{game_number}',
+            'url': f'https://commonologygame.com/award_certificate/{game_number}',
             'game_number': game_number
         }
         msg = render_to_string('game/email/winner_email.html', email_context).replace("\n", "")
-        send_one(winner, 'You earned a coffee mug!', msg)
+        send_one(winner, 'You are a winner in this weeks game!', msg)
     except Exception as e:
         logger.exception(f"Could not send winner notification email {e}")
         slackit(f"{winner} won the game but something went wrong with the notification email.")
