41be232b6e6cebeedddc40fcfc677deabf4b8964
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-10-21 13:55:32 -0400

    Added send_winner_notice()

--- game/mail.py
+++ game/mail.py
@@ -15,7 +15,7 @@ def send_winner_notice(winner, game_number):
             'game_number': game_number
         }
         msg = render_to_string('game/email/winner_email.html', email_context).replace("\n", "")
-        send_one(winner, 'You earned a coffee mug!', game_number)
+        send_one(winner, 'You earned a coffee mug!', msg)
     except Exception as e:
         logger.exception(f"Could not send winner notification email {e}")
         slackit(f"{winner} won the game but something went wrong with the notification email.")
