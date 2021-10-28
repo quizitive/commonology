0544f6a69fbe1b6e4275d6480fd2eebb5bcc77f1
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-10-28 17:49:00 -0400

    Send prior winners award certificates.

--- game/mail.py
+++ game/mail.py
@@ -1,5 +1,7 @@
+import posixpath
 from django.template.loader import render_to_string
 from project.utils import slackit
+from project.settings import DOMAIN, WINNER_ROOT
 from mail.utils import send_one
 
 import logging
@@ -19,3 +21,16 @@ def send_winner_notice(winner, game_number):
     except Exception as e:
         logger.exception(f"Could not send winner notification email {e}")
         slackit(f"{winner} won the game but something went wrong with the notification email.")
+
+
+def send_prior_winner_notice(winner, game_number, filename):
+    slackit(f"Sending winner notification to {winner} for game {game_number}.")
+
+    url = posixpath.join(f'https://{DOMAIN}', WINNER_ROOT, filename)
+    try:
+        email_context = {'url': url, 'game_number': game_number}
+        msg = render_to_string('game/email/winner_email_prior.html', email_context).replace("\n", "")
+        send_one(winner, 'Award Certificate', msg)
+    except Exception as e:
+        logger.exception(f"Could not send winner notification email {e}")
+        slackit(f"{winner} won game {game_number} but something went wrong with the notification email.")
