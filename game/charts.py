43a5b6fc5f6aca0cc6340a410187b4721b67e954
tedsmoore <tedsmoore@gmail.com>
2021-11-02 11:37:35 -0400

    add new players to admin charts

--- game/charts.py
+++ game/charts.py
@@ -2,6 +2,7 @@ from functools import lru_cache
 
 from pychartjs import ChartType, Color, Options, BaseSmartChart, BaseChartData
 from game.models import Game
+from game.utils import new_players_for_game
 
 
 class PlayerTrendChart(BaseSmartChart):
@@ -18,16 +19,22 @@ class PlayerTrendChart(BaseSmartChart):
 class PlayersAndMembersDataset:
 
     def __init__(self, **kwargs):
+        self.Players = GamePlayerCount(
+            player_filters={},
+            borderColor=Color.Hex(0x0095daFF),
+            backgroundColor=Color.Hex(0x0095daFF),
+            **kwargs
+        )
         self.Members = GamePlayerCount(
             player_filters={'player__is_member': True},
             borderColor=Color.Hex(0xf26649FF),
             backgroundColor=Color.Hex(0xf26649FF),
             **kwargs
         )
-        self.Players = GamePlayerCount(
-            player_filters={},
-            borderColor=Color.Hex(0x0095daFF),
-            backgroundColor=Color.Hex(0x0095daFF),
+        self.NewPlayers = GamePlayerCount(
+            numerator_fcn='new_players',
+            borderColor=Color.Hex(0x237073FF),
+            backgroundColor=Color.Hex(0x237073FF),
             **kwargs
         )
 
@@ -36,10 +43,14 @@ class PlayersAndMembersDataset:
 
 
 class GamePlayerCount(BaseChartData):
+    # self.numerator_fcn is the name of the desired numerator function that
+    # takes the argument this_period, which is a list of games in a period,
+    # and returns a list of numerators that ultimately get returned to the chart
 
     def __init__(self, slug, since_game=0, player_filters=None, agg_period=1, **kwargs):
         self.slug = slug
         self.since_game = since_game
+        self.numerator_fcn = 'players_with_filters'
         self.player_filters = player_filters or {}
         self.agg_period = agg_period
         super().__init__(**kwargs)
@@ -63,12 +74,19 @@ class GamePlayerCount(BaseChartData):
         for p in self.periods():
             this_period = games.filter(game_id__gte=p[0], game_id__lte=p[1])
             try:
-                this_pd_avg = sum([g.players_dict.filter(**self.player_filters).count() for g in this_period]) / this_period.count()
+                numerator_fcn = self.__getattribute__(self.numerator_fcn)
+                this_pd_avg = sum(numerator_fcn(this_period)) / this_period.count()
             except ZeroDivisionError:
                 break
             data.append(this_pd_avg)
         data.reverse()
         return data
+
+    def players_with_filters(self, this_period):
+        return [g.players_dict.filter(**self.player_filters).count() for g in this_period]
+
+    def new_players(self, this_period):
+        return [new_players_for_game(g.series.slug, g.game_id).count() for g in this_period]
 
     def get_labels(self):
         periods = self.periods()
