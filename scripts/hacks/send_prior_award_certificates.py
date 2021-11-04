79ba53a99176f2c39bfa0ce96463929ace16235b
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-11-04 14:05:57 -0400

    Add history log of Award Certficates sent to players.

--- scripts/hacks/send_prior_award_certificates.py
+++ scripts/hacks/send_prior_award_certificates.py
@@ -1,6 +1,7 @@
 
 import sys
 import os
+import datetime
 import django
 
 path = os.getcwd()
@@ -12,6 +13,7 @@ from users.models import Player
 from game.utils import write_winner_certificate
 from game.mail import send_prior_winner_notice
 from project.utils import our_now
+from users.utils import player_log_entry
 
 data = '''64,Emma Himes
 64,Ryan Murphy
@@ -218,12 +220,15 @@ def do(winner, game_number):
     send_prior_winner_notice(winner, game_number, filename)
 
 
-marc = Player.objects.get(email='ms@koplon.com')
-do(marc, 1)
+# marc = Player.objects.get(email='ms@koplon.com')
+# do(marc, 1)
 
 for email, game_number in data:
     players = Player.objects.filter(email=email).all()
     for winner in players:
         if winner.subscribed:
             print(f"{winner.display_name},{game_number}")
+            le = player_log_entry(winner, f"Award Certificate sent for game {game_number}.")
+            le.action_time = datetime.datetime(2021, 11, 2, 16, 46, 0)
+            le.save()
             # do(winner, game_number)
