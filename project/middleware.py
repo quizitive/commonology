590cf30ddea4ca52e2c5b75f29809ff2020e1c8a
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-11-08 10:10:36 -0500

    Use middleware to save parameters to session.

--- project/middleware.py
+++ project/middleware.py
@@ -3,7 +3,6 @@
 class ParametersMiddleware():
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
         for key, value in request.GET.items():
