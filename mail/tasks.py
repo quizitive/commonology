aab2f5268e87a8ce0e81eea42cfbb7be5af488c4
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2021-12-03 10:30:27 -0500

    Send thank you email to referrer when anyone plays for the first time.

--- mail/tasks.py
+++ mail/tasks.py
@@ -1,7 +1,13 @@
 from celery import shared_task
+from django.template.loader import render_to_string
 from mail.utils import sendgrid_send
 
 
 @shared_task(queue='serial')
 def mail_task(subject, msg, email_list, top_components=(), bottom_components=()):
     sendgrid_send(subject, msg, email_list, top_components=top_components, bottom_components=bottom_components)
+
+
+def send_refer_thankyou(player):
+    msg = render_to_string('mail/refer_thankyou_email.html', {'player': player.name})
+    return mail_task("Thank you for the referral.", msg, [(player.referrer.email, None)])
