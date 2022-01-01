0ce9cfbdcd1485e480278ff0117ba57d0ee04b90
tedsmoore <tedsmoore@gmail.com>
2022-01-01 14:29:14 -0500

    persist search query when filters are used

--- leaderboard/static/leaderboard/js/leaderboard.js
+++ leaderboard/static/leaderboard/js/leaderboard.js
@@ -1,4 +1,4 @@
-function filter_focus(elm) {
+function filterFocus(elm) {
     $('div.filter-button').removeClass('cg-blue').addClass('w3-light-grey')
     $(elm).addClass("selected").addClass('cg-blue').removeClass('w3-light-grey')
 }
