403865ff952723fe1d189a5a73ae5f5c8b3d2c24
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-01-07 15:04:07 -0500

    Player_id arg rather than player arg for `save_last_visit_t()` task.

--- leaderboard/tasks.py
+++ leaderboard/tasks.py
@@ -1,7 +1,9 @@
 from celery import shared_task
+from users.models import Player
 
 
 @shared_task(queue='serial')
-def save_last_visit_t(player, key, value):
+def save_last_visit_t(player_id, key, value):
+    player = Player.objects.get(id=player_id)
     player.data[key] = value
     player.save()
