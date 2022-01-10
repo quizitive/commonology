71f70dd7e953ac70f3914bee1337789a5f32433b
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-01-10 16:27:05 -0500

    Renamed settings.BROKER_URL to settings.CELERY_BROKER_URL. (#645)
    * Celery server problems in production - remove delay for now.
    
    * Fixed settings.BROKER_URL now named settings.CELERY_BROKER_URL.
    
    * Fixed settings.BROKER_URL now named settings.CELERY_BROKER_URL.

--- leaderboard/tasks.py
+++ leaderboard/tasks.py
@@ -2,7 +2,7 @@ from celery import shared_task
 from users.models import Player
 
 
-@shared_task(queue='serial')
+@shared_task()
 def save_last_visit_t(player_id, key, value):
     player = Player.objects.get(id=player_id)
     player.data[key] = value
