d97122d6a66d03a7348c47f927d9c6d69e74f797
tedsmoore <tedsmoore@gmail.com>
2022-01-26 15:16:40 -0500

    tweaking everything but I think it's working

--- charts/views.py
+++ charts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-from charts.charts import CHARTS
+from charts.charts import Charts
 
 
-def htmx_chart_view(request, chart_name, *args, **kwargs):
-    chart = CHARTS[chart_name]
+def htmx_chart_view(request, chart_name):
+    chart = Charts.get_chart_class(chart_name)(**{k: v for k, v in request.GET.items()})
     return render(request, "charts/simple_chart.html", {"chart": chart})
