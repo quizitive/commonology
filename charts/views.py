486e00c9136f9a54c4663925cc98b8cbf5d35764
tedsmoore <tedsmoore@gmail.com>
2022-01-27 09:43:53 -0500

    example url

--- charts/views.py
+++ charts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from charts.charts import Charts
 
 
+# e.g. /charts/htmx/your_chart_name/?query=params&passed=as&kwargs=tochart
 def htmx_chart_view(request, chart_name):
     chart = Charts.get_chart_class(chart_name)(**{k: v for k, v in request.GET.items()})
     return render(request, "charts/simple_chart.html", {"chart": chart})
