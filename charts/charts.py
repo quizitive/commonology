7777e82e72c8ea07baa98794ecbfcf9106a6d290
tedsmoore <tedsmoore@gmail.com>
2022-01-26 21:00:55 -0500

    functioning lazy charts with Apex library

--- charts/charts.py
+++ charts/charts.py
@@ -1,19 +1,23 @@
 from urllib import parse
 
-from game.charts import PlayerTrendChart
+from game.charts import GamePlayerTrendChart
 
 
 class Charts:
-    weekly_players = "weekly_players"
+    """A container class for accessing available charts"""
+
+    # Define charts here, be sure to add to get_chart_class() below
+    game_player_trend = "game_player_trend"
+
+    @classmethod
+    def get_chart_class(cls, chart_name):
+        """Lookup charts by their name"""
+        charts = {
+            cls.game_player_trend: GamePlayerTrendChart
+        }
+        return charts[chart_name]
 
     @classmethod
     def lazy_chart(cls, chart_name, **kwargs):
         url_fmt = parse.urlencode(kwargs)
         return f"{chart_name}?{url_fmt}"
-
-    @classmethod
-    def get_chart_class(cls, chart_name):
-        charts = {
-            cls.weekly_players: PlayerTrendChart
-        }
-        return charts[chart_name]
