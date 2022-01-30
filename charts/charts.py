6749edafbb9e402979af9f26a26f485d30d0e6cb
tedsmoore <tedsmoore@gmail.com>
2022-01-30 18:05:40 -0500

    function htmx protected view with query param validation

--- charts/charts.py
+++ charts/charts.py
@@ -1,23 +1,12 @@
-from urllib import parse
-
 from game.charts import GamePlayerTrendChart
 
 
 class Charts:
     """A container class for accessing available charts"""
 
-    # Define charts here, be sure to add to get_chart_class() below
-    game_player_trend = "game_player_trend"
+    # Define charts here
+    game_player_trend = GamePlayerTrendChart
 
     @classmethod
-    def get_chart_class(cls, chart_name):
-        """Lookup charts by their name"""
-        charts = {
-            cls.game_player_trend: GamePlayerTrendChart
-        }
-        return charts[chart_name]
-
-    @classmethod
-    def lazy_chart(cls, chart_name, **kwargs):
-        url_fmt = parse.urlencode(kwargs)
-        return f"{chart_name}?{url_fmt}"
+    def get_chart(cls, chart_name):
+        return getattr(cls, chart_name)
