c713f4395874bcc4d91a57d4ab4177d8edd49238
tedsmoore <tedsmoore@gmail.com>
2022-01-30 18:08:25 -0500

    pep8

--- charts/views.py
+++ charts/views.py
@@ -12,4 +12,3 @@ class HTMXChartView(HTMXProtectedView):
         chart_name = kwargs.get("chart_name")
         chart = Charts.get_chart(chart_name)(**{k: v for k, v in request.GET.items()})
         return render(request, "charts/simple_chart.html", {"chart": chart})
-
