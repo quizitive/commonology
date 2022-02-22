5d2de34d0d6ee52fa3ad664c1cb6b2f926ec18a4
tedsmoore <tedsmoore@gmail.com>
2022-02-22 14:41:07 -0500

    cleaning up unused methods

--- components/templatetags/component_tags.py
+++ components/templatetags/component_tags.py
@@ -10,9 +10,3 @@ register = template.Library()
 def sponsor_component_cards():
     context = {"components": SponsorComponent.active_sponsor_components()}
     return render_to_string('components/card_component_stream.html', context=context)
-
-
-@register.simple_tag
-def sponsor_components_simple():
-    context = {"components": SponsorComponent.active_sponsor_components()}
-    return render_to_string('components/simple_component_stream.html', context=context)
