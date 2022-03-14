72ed22471c4da0bc6489791a7eb07bce399346ae
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-03-14 17:46:11 -0400

    Removed comments from users.authy.py.

--- users/auth.py
+++ users/auth.py
@@ -4,9 +4,6 @@ from users.models import Player
 
 
 class PlayerBackend(ModelBackend):
-    # A player can be authenticated if their referrer code matches the one in their session
-    # and their PendingEmail record is less than 1 hour old.
-    # If a user's session is already logged in then we don't have to do this validation.
 
     def authenticate(self, request, **kwargs):
         user_id = request.session.get('user_id')
