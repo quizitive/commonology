db7d1217ed3371cb6a86de767e3cea0778bf2836
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-03-24 15:19:07 -0400

    Fixed mail test.

--- mail/tests.py
+++ mail/tests.py
@@ -25,7 +25,7 @@ class MassMailTests(BaseGameDataTestCase):
 
     def test_bad_series_test(self):
         mail.outbox = []
-        n, log_msg = mass_mail(self.mm)
+        n, log_msg, batch_id = mass_mail(self.mm)
         self.assertEqual(n, 30)
         self.assertEqual(len(mail.outbox), 1)
 
