d61da5728cd418a4482536c7704b11b9639540c4
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-04-21 10:55:35 -0400

    Added canceled to maillog admin list. (#796)

--- mail/admin.py
+++ mail/admin.py
@@ -97,7 +97,7 @@ class MailLogAdmin(DjangoObjectActions, admin.ModelAdmin):
     cancel_send.label = 'Cancel Message'
     cancel_send.short_description = "Tries to stop message at SendGrid."
 
-    list_display = ('subject', 'sent_date', 'scheduled')
+    list_display = ('subject', 'sent_date', 'scheduled', 'canceled')
     list_filter = ('sent_date',)
     search_fields = ('subject',)
     ordering = ('-sent_date',)
