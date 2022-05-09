496222e3e26d7726b795683d8bdca0b058666120
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-05-09 09:58:56 -0400

    Sanitizing hacks.

--- scripts/hacks/makekey.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-from django.core.management.utils import get_random_secret_key
-print(get_random_secret_key())
--- scripts/hacks/unsubscribe_all.py
+++
@@ -1,6 +0,0 @@
-from users.models import Player
-
-qs = Player.objects.exclude(email='ms@quizitive.com').all()
-for u in qs:
-    u.subscribed = False
-    u.save()
