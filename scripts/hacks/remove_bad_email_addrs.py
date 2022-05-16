15e8e8d5953f1f4d6b87179040584490e3687717
Marc Schwarzschild <ms@TheBrookhavenGroup.com>
2022-05-16 13:20:27 -0400

    Adding hacks back after sanitizing them.

---
+++ scripts/hacks/remove_bad_email_addrs.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+
+
+bad_addrs = '''
+themickman&@gmail.com
+'''
+
+bad_addrs = bad_addrs.split()
+
+User = get_user_model()
+
+qs = User.objects.filter(email__in=bad_addrs).all()
+for u in qs:
+    print(u.email)
+    u.subscribed = False
+    u.is_active = False
+    u.save()
